6c34c6ed9dd3b763d90acfee901cb17b65acf863
romicons <romina.constantinoff@gmail.com>
2024-07-22 16:52:40 -0300

    Started designed the navbar.

---
+++ src/theme/theme.js
@@ -0,0 +1,19 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    primary: {
+      50: '#f5e8ff',
+      100: '#ebd1ff',
+      200: '#e0baff',
+      300: '#d5a3ff',
+      400: '#ca8cff',
+      500: '#bf75ff',
+      600: '#b45eff',
+      700: '#a947ff',
+      800: '#9e30ff',
+      900: '#9319ff',
+      A100: '#8802ff',
+    },
+  },
+})
